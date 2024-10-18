a86187a7904e7f571bca66ec53972e9b5df78209
okkun <okuda4645@gmail.com>
2024-10-19 00:24:15 +0900

    callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const increment = () => setCount((count) => count + 1);
+  const increment = useCallback(() => setCount((c) => c + 1), []);
 
   return (
     <>
