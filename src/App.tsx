0ed2306a0fdda163114bd3e4f847bf4dd708ae3b
okkun <okuda4645@gmail.com>
2024-10-20 17:47:39 +0900

    タイトルを変更

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>PR-Agent Sample</h1>
       <div className="card">
         <Button label="increment" onClick={increment} />
         <Button label="decrement" onClick={decrement} />
